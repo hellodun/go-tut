ea3d30199d3d08fb308095485165b1bd2c73d510
Duncan G <dgatuthu@gmail.com>
2022-05-03 12:37:49 +0300

    Arrays and Slices

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() {
 	conferenceName := "Go conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v tickets are still remaining\n", conferenceTickets, remainingTickets)
@@ -29,9 +30,12 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets -= userTickets  
+	bookings = append(bookings, firstaName + " " + lastName)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v. \n", firstaName, lastName, userTickets, email)
 
 	fmt.Printf("%v ticket are still available for %v. \n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings %v \n", bookings)
 
 }
